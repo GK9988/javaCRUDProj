52848b1097deebfbe7dd159ca0f4dc798e6f454c
Garv Khurana <gk12345work@gmail.com>
2025-02-19 13:22:40 +0530

    JWT

--- src/main/java/com/garvk/CrudProj/controllers/UserController.java
+++ src/main/java/com/garvk/CrudProj/controllers/UserController.java
@@ -1,10 +1,14 @@
 package com.garvk.CrudProj.controllers;
 
 import com.garvk.CrudProj.models.User;
+import com.garvk.CrudProj.services.JwtService;
 import com.garvk.CrudProj.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,11 +19,32 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     @Autowired
-    UserService userService;
+    private UserService userService;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private JwtService jwtService;
 
     @PostMapping("register")
     public ResponseEntity<?> addUser(@RequestBody User user){
         User aOutUser = userService.addUser(user);
         return new ResponseEntity<>(aOutUser, HttpStatus.CREATED);
+    }
+
+    @PostMapping("login")
+    public String login(@RequestBody User user){
+        Authentication authentication = authenticationManager
+                .authenticate(
+                        new UsernamePasswordAuthenticationToken(
+                                user.getUsername(),
+                                user.getPassword()));
+
+        if(authentication.isAuthenticated()){
+            return jwtService.generateToken(user.getUsername());
+        } else {
+            return "Login Failed";
+        }
     }
 }
---
+++ src/main/java/com/garvk/CrudProj/services/JwtService.java
@@ -0,0 +1,85 @@
+package com.garvk.CrudProj.services;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import java.security.Key;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+@Service
+public class JwtService {
+
+    private String SECRET_KEY;
+
+    public JwtService(){
+        SECRET_KEY = generateSecretKey();
+    }
+
+    public String generateSecretKey(){
+        try{
+            KeyGenerator keyGen = KeyGenerator.getInstance("HmacSHA256");
+            SecretKey secretKey = keyGen.generateKey();
+            System.out.println("Secret Key: " + secretKey.toString());
+            return Base64.getEncoder().encodeToString(secretKey.getEncoded());
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public String generateToken(String aInUsername) {
+        Map<String, Object> claims = new HashMap<>();
+
+        return Jwts.builder()
+                .claims(claims)
+                .subject(aInUsername)
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + 1000*60*3))
+                .signWith(getKey(), SignatureAlgorithm.HS256).compact();
+    }
+
+    private SecretKey getKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public String extractUserName(String aInToken){
+        return extractClaim(aInToken, Claims::getSubject);
+    }
+
+    private <T> T extractClaim(String aInToken, Function<Claims, T> claimResolver){
+        final Claims claims = extractAllClaims(aInToken);
+        return claimResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String aInToken) {
+        return Jwts.parser()
+                .verifyWith(getKey())
+                .build()
+                .parseSignedClaims(aInToken).getPayload();
+    }
+
+    public boolean validateToken(String aInToken, UserDetails userDetails) {
+        final String userName = extractUserName(aInToken);
+        return (userName.equals(userDetails.getUsername()) && !isTokenExpired(aInToken));
+    }
+
+    private boolean isTokenExpired(String aInToken){
+        return extractExpiration(aInToken).before(new Date());
+    }
+
+    private Date extractExpiration(String aInToken){
+        return extractClaim(aInToken, Claims::getExpiration);
+    }
+}
